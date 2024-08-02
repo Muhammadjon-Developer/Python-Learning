bb99f100f9837ce87b08f8dbb60c4807484d72e2
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-08-02 16:22:06 +0300

    python print funksiyasi

---
+++ main.py
@@ -0,0 +1,7 @@
+# print() function ni bizga terminalda yoki da boshqa joyda yozgan ma'lumotimizni ko'rsatadi
+
+print(10, 'Hello', True) # javob: 10 Hello True
+print("Salom mening ismim Muhammadjon")
+
+print(print)  #javob: <built-in function print> = print bu python ning shaxsiy functioni dir
+print(input)  #javob: <built-in function input> = input bu python ning shaxsiy functioni dir
