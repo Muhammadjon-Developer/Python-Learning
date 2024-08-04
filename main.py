415b274ba678ad6424b28f608fff7e5590ba00b4
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-08-04 13:58:49 +0300

    input, capitalize va upper funksiyalari

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 # print() function ni bizga terminalda yoki da boshqa joyda yozgan ma'lumotimizni ko'rsatadi
 
-print(10, 'Hello', True) # javob: 10 Hello True
-print("Salom mening ismim Muhammadjon")
+# print(10, 'Hello', True) # javob: 10 Hello True
+# print("Salom mening ismim Muhammadjon")
 
-print(print)  #javob: <built-in function print> = print bu python ning shaxsiy functioni dir
-print(input)  #javob: <built-in function input> = input bu python ning shaxsiy functioni dir
+# print(print)  #javob: <built-in function print> = print bu python ning shaxsiy functioni dir
+# print(input)  #javob: <built-in function input> = input bu python ning shaxsiy functioni dir
+
+# INPUT FUNCTION NI BILAN ISHLASH
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# job = input("Enter your job: ")
+
+# print(name, age, job) # inputga yozgan ma'lumotlaringizni ko'rsatadi
+
+# CAPITALIZE
+# ism = input("Ismingiz nima: ")
+# print(ism.capitalize()) # Qanday uslubda ma'lumot kiritmang ning bosh harifini katta 
+# print(ism) # Asl o'zgaruvchining qiymatini o'zgartirmaydi yangi qiymat yaratadi capatalize va boshqa ko'plab funksiyalar
+
+# UPPER
+hobby = input("Enter your hobby: ")
+print(hobby.upper()) # Kiritgan barcha ma'lumotlarni katta harfda ko'rsatadi
